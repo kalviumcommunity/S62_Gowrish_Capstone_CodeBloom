ad95cd98bb116aafc0c2d5e2318c6023f5792075
unknown <Gowrishankar.s@btech.christuniversity.in>
2025-05-20 20:28:08 +0530

    init_front

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,17 @@
-import React from 'react'
-import LandingPage from './pages/LandingPage'
-import SignupPage from './pages/SignUp'
-import LoginPage from './pages/LoginPage'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LandingPage from "./pages/LandingPage";
+import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignUp";
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path = '/' element = { <LandingPage />} />
-        <Route path = '/login' element = { <LoginPage />} />
-        <Route path = '/signup' element = { <SignupPage />} />
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/login" element={<LoginPage />} />
       </Routes>
     </Router>
-    
-  )
+  );
 }
 
-export default App
+export default App;
